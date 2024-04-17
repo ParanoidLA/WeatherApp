c88c1db0a07df77f1c7ca5f309b2eacbe253e67e
ParanoidLA <paranoidla04@gmail.com>
2024-04-17 16:02:47 +0530

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Lakshya Agrawal on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
